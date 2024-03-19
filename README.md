51cb4ee1fdccb49055146aae9f69e8fbf4954d81
Віталій Чолан <vitalikcolan@gmail.com>
2024-03-19 11:10:07 +0200

    Added Prettier for formatting code

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # Custom Login Form
 
 ## Overview:
+
 This project is a custom login form implemented using HTML, CSS, and JavaScript. It provides a simple and intuitive interface for users to sign up and sign in to an application.
 
 ## Features:
-* Sign Up Functionality: Users can input their full name, email, username, and password to sign up for an account.
-* Validation: The form incorporates validation for each input field to ensure data integrity.
-* Password Strength Check: Password strength is assessed based on criteria such as length, inclusion of uppercase and lowercase letters, numbers, and symbols.
-* Password Confirmation: Users are prompted to confirm their password to minimize errors during sign-up.
-* Terms Agreement: A checkbox is provided for users to agree to the terms of use before signing up.
-* Responsive Design: The form layout adjusts according to different screen sizes, ensuring a seamless user experience across devices.
+
+- Sign Up Functionality: Users can input their full name, email, username, and password to sign up for an account.
+- Validation: The form incorporates validation for each input field to ensure data integrity.
+- Password Strength Check: Password strength is assessed based on criteria such as length, inclusion of uppercase and lowercase letters, numbers, and symbols.
+- Password Confirmation: Users are prompted to confirm their password to minimize errors during sign-up.
+- Terms Agreement: A checkbox is provided for users to agree to the terms of use before signing up.
+- Responsive Design: The form layout adjusts according to different screen sizes, ensuring a seamless user experience across devices.
 
 ## Technologies Used:
-* HTML: Markup language used to structure the form elements.
-* CSS: Styling language utilized for visual presentation and layout.
-* JavaScript: Programming language employed for form validation and interactivity.
-* Font Awesome: Icon library utilized for decorative icons within the form.
+
+- HTML: Markup language used to structure the form elements.
+- CSS: Styling language utilized for visual presentation and layout.
+- JavaScript: Programming language employed for form validation and interactivity.
+- Font Awesome: Icon library utilized for decorative icons within the form.
 
 ## Contribution Guidelines:
+
 Contributions to enhance the project are welcome. To contribute, follow these steps:
 
 1. Fork the repository.
@@ -27,8 +31,10 @@ Contributions to enhance the project are welcome. To contribute, follow these st
 5. Create a new Pull Request.
 
 ## Author:
+
 Cholan Vitaliy - [My GitHub Profile](https://github.com/VitalikCholan)
 
 ## Acknowledgments:
-* Special thanks to Font Awesome for providing the icons used in this project.
-* Inspiration for this project was drawn from various login form designs and tutorials available online.
+
+- Special thanks to Font Awesome for providing the icons used in this project.
+- Inspiration for this project was drawn from various login form designs and tutorials available online.
--- js/script.js
+++ js/script.js
@@ -1,110 +1,112 @@
-const form = document.querySelector('#form');
+const form = document.querySelector("#form");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const inputs = document.querySelectorAll('input');
-    const errorMessages = document.querySelectorAll('.error');
-    const successIcons = document.querySelectorAll('.success-icon');
-    const failureIcons = document.querySelectorAll('.failure-icon');
+  const inputs = document.querySelectorAll("input");
+  const errorMessages = document.querySelectorAll(".error");
+  const successIcons = document.querySelectorAll(".success-icon");
+  const failureIcons = document.querySelectorAll(".failure-icon");
 
-    const fullNameRegex = /^[a-zA-z]+(?:['’][a-zA-Z]+)*(?:[-][a-zA-Z]+)* [a-zA-Z]+(?:['’][a-zA-Z]+)*(?:[-][a-zA-Z]+)* [a-zA-Z]+(?:['’][a-zA-Z]+)*(?:[-][a-zA-Z]+)*$/;
-    const emailRegex = /^([a-z0-9\.-]+)@([a-z0-9-]+)\.([a-z]{3,8})$/;
+  const fullNameRegex =
+    /^[a-zA-z]+(?:['’][a-zA-Z]+)*(?:[-][a-zA-Z]+)* [a-zA-Z]+(?:['’][a-zA-Z]+)*(?:[-][a-zA-Z]+)* [a-zA-Z]+(?:['’][a-zA-Z]+)*(?:[-][a-zA-Z]+)*$/;
+  const emailRegex = /^([a-z0-9\.-]+)@([a-z0-9-]+)\.([a-z]{3,8})$/;
 
-    //For password validation
-    const lowercaseRegex = /[a-z]/;
-    const uppercaseRegex = /[A-Z]/;
-    const numberRegex = /[0-9]/;
-    const symbolRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+  //For password validation
+  const lowercaseRegex = /[a-z]/;
+  const uppercaseRegex = /[A-Z]/;
+  const numberRegex = /[0-9]/;
+  const symbolRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
 
-    if(inputs[0].value.trim() === '' || inputs[0].value.trim() === null){
-        errorMessages[0].innerText = 'Full name is required';
-        successIcons[0].style.opacity = '0';
-        failureIcons[0].style.opacity = '1';
-    } else if(!fullNameRegex.test(inputs[0].value)){
-        errorMessages[0].innerText = 'Provide a full name';
-        successIcons[0].style.opacity = '0';
-        failureIcons[0].style.opacity = '1';
-    } else{
-        errorMessages[0].innerText = '';
-        successIcons[0].style.opacity = '1';
-        failureIcons[0].style.opacity = '0';
-    }
+  if (inputs[0].value.trim() === "" || inputs[0].value.trim() === null) {
+    errorMessages[0].innerText = "Full name is required";
+    successIcons[0].style.opacity = "0";
+    failureIcons[0].style.opacity = "1";
+  } else if (!fullNameRegex.test(inputs[0].value)) {
+    errorMessages[0].innerText = "Provide a full name";
+    successIcons[0].style.opacity = "0";
+    failureIcons[0].style.opacity = "1";
+  } else {
+    errorMessages[0].innerText = "";
+    successIcons[0].style.opacity = "1";
+    failureIcons[0].style.opacity = "0";
+  }
 
-    if(inputs[1].value.trim() === '' || inputs[1].value.trim() === null){
-        errorMessages[1].innerText = 'Email is required';
-        successIcons[1].style.opacity = '0';
-        failureIcons[1].style.opacity = '1';
-    } else if(!emailRegex.test(inputs[1].value)){
-        errorMessages[1].innerText = 'Provide a valid email address';
-        successIcons[1].style.opacity = '0';
-        failureIcons[1].style.opacity = '1';
-    } else{
-        errorMessages[1].innerText = '';
-        successIcons[1].style.opacity = '1';
-        failureIcons[1].style.opacity = '0';
-    }
+  if (inputs[1].value.trim() === "" || inputs[1].value.trim() === null) {
+    errorMessages[1].innerText = "Email is required";
+    successIcons[1].style.opacity = "0";
+    failureIcons[1].style.opacity = "1";
+  } else if (!emailRegex.test(inputs[1].value)) {
+    errorMessages[1].innerText = "Provide a valid email address";
+    successIcons[1].style.opacity = "0";
+    failureIcons[1].style.opacity = "1";
+  } else {
+    errorMessages[1].innerText = "";
+    successIcons[1].style.opacity = "1";
+    failureIcons[1].style.opacity = "0";
+  }
 
-    if(inputs[2].value.trim() === '' || inputs[2].value.trim() === null){
-        errorMessages[2].innerText = 'Username is required';
-        successIcons[2].style.opacity = '0';
-        failureIcons[2].style.opacity = '1';
-    } else{
-        errorMessages[2].innerText = '';
-        successIcons[2].style.opacity = '1';
-        failureIcons[2].style.opacity = '0';
-    }
+  if (inputs[2].value.trim() === "" || inputs[2].value.trim() === null) {
+    errorMessages[2].innerText = "Username is required";
+    successIcons[2].style.opacity = "0";
+    failureIcons[2].style.opacity = "1";
+  } else {
+    errorMessages[2].innerText = "";
+    successIcons[2].style.opacity = "1";
+    failureIcons[2].style.opacity = "0";
+  }
 
-    if(inputs[3].value.trim() === '' || inputs[3].value.trim() === null){
-        errorMessages[3].innerText = 'Password is required';
-        successIcons[3].style.opacity = '0';
-        failureIcons[3].style.opacity = '1';
-    } else if(inputs[3].value.length < 8){
-        errorMessages[3].innerText = 'Password must be at least 8 characters long';
-        successIcons[3].style.opacity = '0';
-        failureIcons[3].style.opacity = '1';
-    } else if(!lowercaseRegex.test(inputs[3].value)){
-        errorMessages[3].innerText = 'Password must contain at least 1 lowercase letter';
-        successIcons[3].style.opacity = '0';
-        failureIcons[3].style.opacity = '1';
-    } else if(!uppercaseRegex.test(inputs[3].value)){
-        errorMessages[3].innerText = 'Password must contain at least 1 uppercase letter';
-        successIcons[3].style.opacity = '0';
-        failureIcons[3].style.opacity = '1';
-    } else if(!numberRegex.test(password.value)){
-        errorMessages[3].innerText = 'Password must contain at least one number';
-        successIcons[3].style.opacity = '0';
-        failureIcons[3].style.opacity = '1';
-    } else if(!symbolRegex.test(inputs[3].value)){
-        errorMessages[3].innerText = 'Password must contain at least one symbol';
-        successIcons[3].style.opacity = '0';
-        failureIcons[3].style.opacity = '1';
-    } else{
-        errorMessages[3].innerText = '';
-        successIcons[3].style.opacity = '1';
-        failureIcons[3].style.opacity = '0';
-    }
+  if (inputs[3].value.trim() === "" || inputs[3].value.trim() === null) {
+    errorMessages[3].innerText = "Password is required";
+    successIcons[3].style.opacity = "0";
+    failureIcons[3].style.opacity = "1";
+  } else if (inputs[3].value.length < 8) {
+    errorMessages[3].innerText = "Password must be at least 8 characters long";
+    successIcons[3].style.opacity = "0";
+    failureIcons[3].style.opacity = "1";
+  } else if (!lowercaseRegex.test(inputs[3].value)) {
+    errorMessages[3].innerText =
+      "Password must contain at least 1 lowercase letter";
+    successIcons[3].style.opacity = "0";
+    failureIcons[3].style.opacity = "1";
+  } else if (!uppercaseRegex.test(inputs[3].value)) {
+    errorMessages[3].innerText =
+      "Password must contain at least 1 uppercase letter";
+    successIcons[3].style.opacity = "0";
+    failureIcons[3].style.opacity = "1";
+  } else if (!numberRegex.test(password.value)) {
+    errorMessages[3].innerText = "Password must contain at least one number";
+    successIcons[3].style.opacity = "0";
+    failureIcons[3].style.opacity = "1";
+  } else if (!symbolRegex.test(inputs[3].value)) {
+    errorMessages[3].innerText = "Password must contain at least one symbol";
+    successIcons[3].style.opacity = "0";
+    failureIcons[3].style.opacity = "1";
+  } else {
+    errorMessages[3].innerText = "";
+    successIcons[3].style.opacity = "1";
+    failureIcons[3].style.opacity = "0";
+  }
 
-    if(inputs[4].value.trim() === '' || inputs[4].value.trim() === null){
-        errorMessages[4].innerText = 'Repeat password is required';
-        successIcons[4].style.opacity = '0';
-        failureIcons[4].style.opacity = '1';
-    } else if(inputs[3].value !== inputs[4].value){
-        errorMessages[4].innerText = "Password doesn't match";
-        successIcons[4].style.opacity = '0';
-        failureIcons[4].style.opacity = '1';
-    } else{
-        errorMessages[4].innerText = '';
-        successIcons[4].style.opacity = '1';
-        failureIcons[4].style.opacity = '0';
-    }
+  if (inputs[4].value.trim() === "" || inputs[4].value.trim() === null) {
+    errorMessages[4].innerText = "Repeat password is required";
+    successIcons[4].style.opacity = "0";
+    failureIcons[4].style.opacity = "1";
+  } else if (inputs[3].value !== inputs[4].value) {
+    errorMessages[4].innerText = "Password doesn't match";
+    successIcons[4].style.opacity = "0";
+    failureIcons[4].style.opacity = "1";
+  } else {
+    errorMessages[4].innerText = "";
+    successIcons[4].style.opacity = "1";
+    failureIcons[4].style.opacity = "0";
+  }
 
-    if(inputs[5].checked){
-        successIcons[5].style.opacity = '1';
-        failureIcons[5].style.opacity = '0';
-    } else {
-        successIcons[5].style.opacity = '0';
-        failureIcons[5].style.opacity = '1';
-    }
+  if (inputs[5].checked) {
+    successIcons[5].style.opacity = "1";
+    failureIcons[5].style.opacity = "0";
+  } else {
+    successIcons[5].style.opacity = "0";
+    failureIcons[5].style.opacity = "1";
+  }
 });
-
